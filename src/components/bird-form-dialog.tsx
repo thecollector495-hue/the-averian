067fd99b5102f6c94b4b4212d997894e83482c38
The Collector <thecollector495@gmail.com>
2025-07-05 23:01:10 +0000

    If this doesn't work maybe we should make them two components that's foc

--- src/components/bird-form-dialog.tsx
+++ src/components/bird-form-dialog.tsx
@@ -215,12 +215,6 @@ export function BirdFormDialog({ isOpen, onOpenChange, onSave, initialData, allB
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
       <DialogContent
         className="sm:max-w-2xl"
-        onPointerDownOutside={(e) => {
-          const target = e.target as HTMLElement;
-          if (target.closest('[data-radix-popper-content-wrapper]')) {
-            e.preventDefault();
-          }
-        }}
       >
         <DialogHeader>
           <DialogTitle>{isEditMode ? 'Edit Bird' : 'Add a New Bird'}</DialogTitle>
@@ -627,5 +621,7 @@ export function BirdFormDialog({ isOpen, onOpenChange, onSave, initialData, allB
     </Dialog>
   );
 }
+
+    
 
     
